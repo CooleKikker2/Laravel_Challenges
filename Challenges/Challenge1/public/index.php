4a7a1629b1c907fe7d0870bb72ee0bccc9d693ae
CooleKikker <pctcoen@gmail.com>
2020-12-17 09:15:56 +0100

    Challenge1

---
+++ Challenges/Challenge1/public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Laravel Challenge 1</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Laravel Challenge 1</h1>
+        <p>Gemaakt door Martijn van Rooijen en Coen Vink.</p>
+        <p>made with laravel</p>
+    </div>
+</body>
+</html>
